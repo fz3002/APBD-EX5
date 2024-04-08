fb98b42de52893c39a23e9cfbf7e5f6e5278a5f1
fz3002 <franc3002@gmail.com>
2024-04-08 17:58:43 +0200

    Init commit

---
+++ Animals/Animal.cs
@@ -0,0 +1,12 @@
+namespace Animals;
+
+public class Animal
+{
+    public int id { get; set; }
+
+    public string name { get; set; }
+
+    public string category { get; set; }
+    public string mass { get; set; }
+    public string color_of_fur { get; set; }
+}
